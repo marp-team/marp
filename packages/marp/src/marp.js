59112c2e553a72340da973d7ffc36c66e850a114
Yuki Hattori <yukihattori1116@gmail.com>
2018-03-28 23:21:09 +0900

    Add Gaia theme (Working in progress)

--- packages/marp/src/marp.js
+++ packages/marp/src/marp.js
@@ -1,5 +1,6 @@
 import Marpit from '@marp-team/marpit'
 import Default from '../themes/default.scss'
+import Gaia from '../themes/gaia.scss'
 
 export default class Marp extends Marpit {
   constructor(...args) {
@@ -7,5 +8,7 @@ export default class Marp extends Marpit {
 
     const defaultTheme = this.themeSet.add(Default)
     this.themeSet.default = defaultTheme
+
+    this.themeSet.add(Gaia)
   }
 }
