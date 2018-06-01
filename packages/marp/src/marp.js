bce8a8a42b882b375ceac700b818944bd8bd2ac8
Yuki Hattori <yuki.hattori@speee.jp>
2018-06-01 17:51:31 +0900

    Enable code block highlighting powered by highlight.js

--- packages/marp/src/marp.js
+++ packages/marp/src/marp.js
@@ -1,4 +1,6 @@
+/* eslint class-methods-use-this: 0 */
 import Marpit from '@marp-team/marpit'
+import highlightjs from 'highlight.js'
 import Default from '../themes/default.scss'
 import Gaia from '../themes/gaia.scss'
 
@@ -6,11 +8,24 @@ export default class Marp extends Marpit {
   constructor(...args) {
     super(...args)
 
+    // Enable table
     this.markdown.enable('table')
+    this.markdown.set({
+      highlight: (code, lang) => this.highlighter(code, lang),
+    })
 
-    const defaultTheme = this.themeSet.add(Default)
-    this.themeSet.default = defaultTheme
-
+    // Add themes
+    this.themeSet.default = this.themeSet.add(Default)
     this.themeSet.add(Gaia)
+  }
+
+  highlighter(code, lang) {
+    if (lang) {
+      if (['text', 'plain'].includes(lang)) return ''
+      if (highlightjs.getLanguage(lang))
+        return highlightjs.highlight(lang, code, true).value
+    }
+
+    return highlightjs.highlightAuto(code).value
   }
 }
