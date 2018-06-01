7990aa61e998b1a87c2aa81d1e3a38872e194fab
Yuki Hattori <yuki.hattori@speee.jp>
2018-06-01 20:45:58 +0900

    Add test cases about the default markdown settings

--- packages/marp/test/marp.js
+++ packages/marp/test/marp.js
@@ -5,6 +5,31 @@ import Marp from '../lib/marp'
 describe('Marp', () => {
   it('extends Marpit', () => assert(new Marp() instanceof Marpit))
 
+  describe('markdown property', () => {
+    const { markdown } = new Marp()
+
+    // TODO: Use cheerio to be disambiguation
+    it('renders breaks as <br> element', () =>
+      assert(
+        markdown
+          .render('hard\nbreak')
+          .replace(/\n/g, '')
+          .includes('hard<br />break')
+      ))
+
+    it('has enabled table syntax', () =>
+      assert(markdown.render('|a|b|\n|-|-|\n|c|d|').includes('<table>')))
+
+    it('converts URL to hyperlink', () =>
+      assert(
+        markdown
+          .render('https://www.google.com/')
+          .includes(
+            '<a href="https://www.google.com/">https://www.google.com/</a>'
+          )
+      ))
+  })
+
   describe('themeSet property', () => {
     const { themeSet } = new Marp()
 
