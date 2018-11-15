ac2278e4ae6fd7e0eceb307c72da02e3fdb6c0a8
Yuki Hattori <yukihattori1116@gmail.com>
2018-11-15 15:58:07 +0900

    Simplized Gatsby template

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,32 +1,19 @@
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Marp: Markdown Presentation Writer',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-sass',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        cssLoaderOptions: {
+          postCssPlugins: [autoprefixer()],
+          camelCase: 'only',
+        },
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
