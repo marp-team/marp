fa166db58cff93f40676b1ea440606f4e3a01891
Yuki Hattori <yukihattori1116@gmail.com>
2018-11-26 18:56:14 +0900

    Renew layout of hero to be simplify

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import logo from './assets/marp-logo.svg'
-import screenshot from './assets/screenshot-web.png'
 import style from './style/hero.module.scss'
 
 console.log(logo)
 
 const Hero: React.FC = () => (
-  <section className={`${style.hero}`}>
-    <div className={`${style.heroBlock} ${style.heroBlockLead}`}>
-      <h1 className={style.logoHeading}>
-        <img alt="Marp" className={style.logo} src={logo} />
-      </h1>
-      <h2>Markdown Presentation Writer</h2>
+  <section className={style.hero}>
+    <div className={style.lead}>
+      <img alt="Marp" className={style.leadLogo} src={logo} />
+      <header className={style.leadHeader}>
+        <h1 className={style.leadName}>Marp</h1>
+        <p className={style.leadDescription}>Markdown Presentation Writer</p>
+      </header>
     </div>
-    <div className={`${style.heroBlock} ${style.heroBlockScreenshot}`}>
-      <img alt="Screenshot" className={style.screenshot} src={screenshot} />
-    </div>
+    <div className={style.screenshot} />
   </section>
 )
 
