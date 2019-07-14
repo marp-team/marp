37d5bbb45db54a85e80d5adbc432d4b1b5811fb7
Yuki Hattori <yukihattori1116@gmail.com>
2019-07-15 01:34:02 +0900

    Stop using symbolic link

--- version.js
+++ version.js
@@ -1,1 +1,18 @@
-./packages/common-scripts/src/version.js
+#!/usr/bin/env node
+/* version.js: marp-team common script for bumping version */
+
+const fs = require('fs')
+const path = require('path')
+
+// Update CHANGELOG.md
+const unreleased = '## [Unreleased]'
+const [date] = new Date().toISOString().split('T')
+const version = `## v${process.env.npm_package_version} - ${date}`
+
+const changelog = path.resolve(process.cwd(), 'CHANGELOG.md')
+const content = fs.readFileSync(changelog, 'utf8')
+
+fs.writeFileSync(
+  changelog,
+  content.replace(unreleased, `${unreleased}\n\n${version}`)
+)
