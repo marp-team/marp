8dac88cf90821f5d378d4898ef4803b15ecc7ff5
Yuki Hattori <yuki.hattori@speee.jp>
2019-10-30 19:37:18 +0900

    Apply shadow to Heading component

--- packages/website/src/components/heading.js.jsx
+++ packages/website/src/components/heading.js.jsx
@@ -5,26 +5,33 @@ export const Heading = ({ children, level = 1, ...props }) => {
   const Tag = `h${level}`
 
   return (
-    <Tag
+    <span
       css={css`
-        background-color: #009bda;
-        background-image: url('/assets/noise.png'),
-          linear-gradient(-30deg, #02669d, #009bda);
-        box-sizing: border-box;
-        clip-path: polygon(1.5em 0, 100% 0, calc(100% - 1.5em) 100%, 0 100%);
-        color: #fff;
-        line-height: 1.2;
-        margin-left: auto;
-        margin-right: auto;
-        max-width: calc(100% - 60px);
-        padding: 0.25em 2em;
-        text-align: center;
-        width: max-content;
-        word-wrap: break-word;
+        display: block;
+        filter: drop-shadow(0 5px 10px rgba(0, 0, 0, 0.15));
       `}
-      {...props}
     >
-      {children}
-    </Tag>
+      <Tag
+        css={css`
+          background-color: #009bda;
+          background-image: url('/assets/noise.png'),
+            linear-gradient(-30deg, #02669d, #009bda);
+          box-sizing: border-box;
+          clip-path: polygon(1.5em 0, 100% 0, calc(100% - 1.5em) 100%, 0 100%);
+          color: #fff;
+          line-height: 1.2;
+          margin-left: auto;
+          margin-right: auto;
+          max-width: calc(100% - 60px);
+          padding: 0.25em 2em;
+          text-align: center;
+          width: max-content;
+          word-wrap: break-word;
+        `}
+        {...props}
+      >
+        {children}
+      </Tag>
+    </span>
   )
 }
