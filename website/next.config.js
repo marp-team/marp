99a136a3a5f031636d648e54c3107c0f754c9bc6
Yuki Hattori <yukihattori1116@gmail.com>
2020-08-22 22:16:31 +0900

    Set correct host in Netlify's deploy preview

--- website/next.config.js
+++ website/next.config.js
@@ -2,8 +2,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: !!process.env.ANALYZE,
 })
 
+const env = { BUILD_YEAR: new Date().getFullYear().toString() }
+
+// for Netlify's deploy preview
+if (process.env.DEPLOY_URL) env.NEXT_PUBLIC_HOST = process.env.DEPLOY_URL
+
 module.exports = withBundleAnalyzer({
-  env: { BUILD_YEAR: new Date().getFullYear().toString() },
+  env,
   webpack: (config) => {
     config.module.rules.push({ test: /\.md$/, use: 'raw-loader' })
     return config
