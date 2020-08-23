04d9317c6a24a09b2b4f4e5610acec6b88bbc4bd
Yuki Hattori <yukihattori1116@gmail.com>
2020-08-24 02:54:12 +0900

    Optimize build for blog pages

---
+++ website/utils/markdown/parse/index.ts
@@ -0,0 +1,18 @@
+import remarkParse from 'remark-parse'
+import remarkSlug from 'remark-slug'
+import unified from 'unified'
+import removePosition from 'unist-util-remove-position'
+import { imageParagraphToFigure } from './image-paragrpah-to-figure'
+
+let parser: unified.Processor | undefined
+
+export const parse = async (md: string) => {
+  parser =
+    parser ||
+    unified()
+      .use(remarkParse, { commonmark: true })
+      .use(remarkSlug)
+      .use(imageParagraphToFigure)
+
+  return removePosition(await parser.run(parser.parse(md)), true)
+}
